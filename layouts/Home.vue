a1245b2098996a80793bdb5391950a0fac5452dc
John Leider <john.j.leider@gmail.com>
2018-01-16 09:33:51 -0500

    removed canariasjs

--- layouts/Home.vue
+++ layouts/Home.vue
@@ -4,37 +4,4 @@
       v-container(fluid).pa-0
         v-slide-x-transition(mode="out-in")
           router-view
-    v-snackbar(
-      :value="snackbar"
-      :timeout="0"
-      top
-    )
-      div Angular vs React vs Vue - CanariasJS
-      v-btn(
-        flat
-        color="amber"
-        href="https://canariasjs.com/"
-        target="_blank"
-      ) Check it out
-      v-btn(flat icon color="white" @click="close").ml-0
-        v-icon close
 </template>
-
-<script>
-  export default {
-    data: () => ({
-      snackbar: false
-    }),
-
-    mounted () {
-      this.snackbar = !localStorage.getItem('__vuetify_canarias_snack')
-    },
-
-    methods: {
-      close () {
-        this.snackbar = false
-        localStorage.setItem('__vuetify_canarias_snack', false)
-      }
-    }
-  }
-</script>
